8868a491c846701c408856cc23b8e310d59eb204
Simone Westphal <hello@swestphal.io>
2021-05-16 17:43:10 +0200

    feature: Register user

--- account/views.py
+++ account/views.py
@@ -13,6 +13,22 @@ def logout_view(request):
     logout(request)
     # Redirect to a success page.
     
+def register(request):
+    if request.method=='POST':
+        user_form=UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # create new user object
+            new_user=user_form.save(commit=FALSE)
+            # set the chosen password
+            new_user.set_password(user_form.cleaned_data['password']) # set is handling hashing
+            # save the user object
+            new_user.save()
+            return render(request,'account/register_done.html',{'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form':user_form})
+
+
 #no more needed - replaced by LoginView.as_view
 # def user_login(request):
 #     if request.method=='POST':
